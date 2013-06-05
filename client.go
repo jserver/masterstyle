6a973aa02ab47642e0a7949d23c03463efb3e65d
Joe Server <joe@jserver.io>
2013-06-05 00:26:12 -0400

    rework launch and if cmd statement

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -18,8 +19,8 @@ type Build struct {
 	Size           string
 	Image          string
 	Key            string
-	Zone           string
-	SecurityGroups []string
+	Placement      string
+	SecurityGroups []ec2.SecurityGroup
 	UserData       string
 	Upgrade        string
 	Group          string
@@ -64,6 +65,7 @@ var (
 	host = flag.String("host", "localhost", "Host to Dial")
 	port = flag.Int("port", 1234, "Port serverstyle is running on")
 
+	reader = bufio.NewReader(os.Stdin)
 	home   = os.Getenv("HOME")
 	config Config
 
@@ -72,31 +74,43 @@ var (
 	results server.Results
 )
 
-func launch() {
-    auth, err := aws.EnvAuth()
-    if err != nil {
-        log.Fatal("AWS AUTH Fail")
-    }
-    e := ec2.New(auth, aws.USEast)
+func launch(build string) {
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		log.Fatal("AWS AUTH Fail")
+	}
+	e := ec2.New(auth, aws.USEast)
 
-	secGroups := []ec2.SecurityGroup{{Name: "django-dev"}, {Name: "serverstyle"}}
+	fmt.Print("Enter Tag Name: ")
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		log.Fatal("Unable to read tag value!")
+	}
+	tags := []ec2.Tag{{"Name", string(line)}}
 
-    options := ec2.RunInstances{
-        ImageId:            "ami-f3d1bb9a",
-        InstanceType:       "t1.micro",
-		KeyName:            "jserver",
-		PlacementGroupName: "us-east-1b",
-		SecurityGroups:     secGroups,
-    }
-    resp, err := e.RunInstances(&options)
-    if err != nil {
-		log.Fatal("AWS ec2 Run Instances Fail")
-    }
+	options := ec2.RunInstances{
+		ImageId:            config.Images[config.Builds[build].Image],
+		InstanceType:       config.Builds[build].Size,
+		KeyName:            config.Builds[build].Key,
+		PlacementGroupName: config.Builds[build].Placement,
+		SecurityGroups:     config.Builds[build].SecurityGroups,
+	}
+	resp, err := e.RunInstances(&options)
+	if err != nil {
+		log.Fatal("AWS ec2 Run Instances Fail", err)
+	}
 
-    for _, instance := range resp.Instances {
-        println("Now running", instance.InstanceId)
-    }
-    println("Make sure you terminate instances to stop the cash flow.")
+	instIds := make([]string, len(resp.Instances))
+	for idx, instance := range resp.Instances {
+		println("Now running", instance.InstanceId)
+		instIds[idx] = instance.InstanceId
+	}
+	_, err = e.CreateTags(instIds, tags)
+	if err != nil {
+		fmt.Println("Error Creating Tags: ", err)
+	}
+
+	println("Make sure you terminate instances to stop the cash flow.")
 }
 
 func main() {
@@ -116,61 +130,65 @@ func main() {
 		log.Fatal("unable to parse config:", err)
 	}
 
-	address := fmt.Sprintf("%s:%d", *host, *port)
-	fmt.Println("Calling: " + address)
-	client, err := rpc.DialHTTP("tcp", address)
-	if err != nil {
-		log.Fatal("dialing:", err)
-	}
-
 	if cmd == "launch" {
-		fmt.Println("GETTING READY TO LAUNCH")
-		launch()
-		fmt.Println("DID WE LAUNCH")
-
-	} else if cmd == "install" {
 		if len(args) != 2 {
-			log.Fatal("No bundle given")
+			log.Fatal("No build given")
 		}
-		bundle := config.Bundles[args[1]]
-		packages := strings.Split(bundle, " ")
-
-		cmdArgs = &server.AptGetArgs{packages}
-		results = new(server.AptGetResults)
-		command = "AptGet.Install"
-
-	} else if cmd == "script" {
-		if len(args) != 2 {
-			log.Fatal("No script given")
-		}
-		script := os.ExpandEnv(args[1])
-		content, err := ioutil.ReadFile(script)
-		if err != nil {
-			log.Fatal("unable to read file")
-		}
-
-		script_parts := strings.Split(script, "/")
-		script_name := script_parts[len(script_parts)-1]
-
-		cmdArgs = &server.ScriptArgs{script_name, content}
-		results = new(server.ScriptResults)
-		command = "Script.Runner"
-
-	} else if cmd == "test" {
-		packages := []string{"A", "B", "C"}
-		cmdArgs = &server.TestArgs{packages}
-		results = new(server.TestResults)
-		command = "Test.Runner"
+		launch(args[1])
 
 	} else {
-		log.Fatal("Command Unknown")
+		address := fmt.Sprintf("%s:%d", *host, *port)
+		fmt.Println("Calling: " + address)
+		client, err := rpc.DialHTTP("tcp", address)
+		if err != nil {
+			log.Fatal("dialing:", err)
+		}
+
+		if cmd == "install" {
+			if len(args) != 2 {
+				log.Fatal("No bundle given")
+			}
+			bundle := config.Bundles[args[1]]
+			packages := strings.Split(bundle, " ")
+
+			cmdArgs = &server.AptGetArgs{packages}
+			results = new(server.AptGetResults)
+			command = "AptGet.Install"
+
+		} else if cmd == "script" {
+			if len(args) != 2 {
+				log.Fatal("No script given")
+			}
+			script := os.ExpandEnv(args[1])
+			content, err := ioutil.ReadFile(script)
+			if err != nil {
+				log.Fatal("unable to read file")
+			}
+
+			script_parts := strings.Split(script, "/")
+			script_name := script_parts[len(script_parts)-1]
+
+			cmdArgs = &server.ScriptArgs{script_name, content}
+			results = new(server.ScriptResults)
+			command = "Script.Runner"
+
+		} else if cmd == "test" {
+			packages := []string{"A", "B", "C"}
+			cmdArgs = &server.TestArgs{packages}
+			results = new(server.TestResults)
+			command = "Test.Runner"
+
+		} else {
+			log.Fatal("Command Unknown")
+		}
+
+		remoteCall := client.Go(command, cmdArgs, results, nil)
+		<-remoteCall.Done
+		errText := results.GetErr()
+		if len(errText) > 0 {
+			fmt.Println(">>> [", errText, "]")
+		}
+		fmt.Println(results.GetOutput())
 	}
 
-	remoteCall := client.Go(command, cmdArgs, results, nil)
-	<-remoteCall.Done
-	errText := results.GetErr()
-	if len(errText) > 0 {
-		fmt.Println(">>> [", errText, "]")
-	}
-	fmt.Println(results.GetOutput())
 }
