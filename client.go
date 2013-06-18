121eb63cb4095c9d725a04841368122875694402
Joe Server <joe@jserver.io>
2013-06-18 11:10:30 -0400

    add a help function

--- client.go
+++ client.go
@@ -120,6 +120,38 @@ func AskMultipleChoice(question string, answers []Answer) string {
 	}
 }
 
+func help() {
+	fmt.Println(`The following commands are available
+	exit,q,quit         Leave Program
+	
+	ls, status          List all machines
+	launch              Create a new ec2 instance from specified build
+	update              apt-get update
+	upgrade             apt-get upgrade
+	install             Install a bundle from json config
+	script              Run a script on the machine specified
+	test                Pass in a few directories to see if ServerStyle is responding
+	easy, easy_install  Install a python bundle from json config
+
+	EC2 Actions on a specified Machine
+	----------------------------------
+	reboot              Reboot specified machine
+	start               Start specified machine
+	stop                Stop specified machine
+	terminate           Terminate specified mach
+
+	S3 Actions
+	----------
+	s3upload            Upload ServerStyle to s3 for new machines
+
+	IP Addresses
+	------------
+	addresses           List all IP Addresses allocated
+	associate           Attach an instance to one of your allocated addresses
+	disassociate        Release an address from an instance
+	`)
+}
+
 func main() {
 	// Config File
 	fmt.Println("Reading config file")
@@ -162,7 +194,7 @@ func main() {
 
 		switch cmd {
 		case "h", "help":
-			fmt.Println("Help Message")
+			help()
 
 		case "exit", "q", "quit":
 			fmt.Println("Bye-Bye")
